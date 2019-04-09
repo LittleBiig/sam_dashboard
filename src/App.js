787411349cd111d2f15d8f242b3789c5517deb21
LittleBiig <16526995+LittleBiig@users.noreply.github.com>
2019-04-09 23:44:26 +0200

    Update directories

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import RobotsOverview from "./components/RobotsOverview";
-import RegistrationForm from "./components/RegistrationForm";
+import RobotsOverview from "./components/RobotsOverviewPage/RobotsOverview";
+import RegistrationForm from "./components/Login/RegistrationForm";
+import LoginForm from "./components/Login/LoginForm";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <LoginForm />
           <RegistrationForm/>
           <RobotsOverview/>
       </div>
