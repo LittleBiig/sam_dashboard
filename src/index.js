10ecef1aba1d22c2e8a2f5a73b9372c8b9a565b4
LittleBiig <16526995+LittleBiig@users.noreply.github.com>
2019-05-28 19:43:35 +0200

    Yarn fixes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './styles/index.scss';
 ReactDOM.render(
     <BrowserRouter>
         <Root />
-</BrowserRouter>, document.getElementById('root'));
+    </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
