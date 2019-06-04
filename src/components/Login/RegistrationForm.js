186138f5dc83abe347a806ba4ae49b75325140c9
LittleBiig <16526995+LittleBiig@users.noreply.github.com>
2019-06-05 00:18:17 +0200

    sign up working

--- src/components/Login/RegistrationForm.js
+++ src/components/Login/RegistrationForm.js
@@ -39,8 +39,22 @@ class RegistrationForm_ extends React.Component {
     }
 
 
-    createNewUser = () => {
-        axios.post(`${API_BASE_URL}${POST_SIGNUP}`, this.state.sign_up)
+    createNewUser = (values) => {
+        const sign_up = {
+                serial_number: values.serial_number,
+                email: values.email,
+                firstname: values.firstname,
+                lastname: values.lastname,
+                password: values.password,
+                office_address: {
+                    street: values.street,
+                    house_nr: values.house_nr,
+                    zip: values.zip,
+                    city: values.city,
+                    country: values.country,
+                },
+            };
+        axios.post(`${API_BASE_URL}${POST_SIGNUP}`, sign_up)
             .then(res => {
                 console.log(res);
             })
@@ -82,9 +96,10 @@ class RegistrationForm_ extends React.Component {
         this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                this.createNewUser(values);
             }
         });
-        this.createNewUser();
+
     }
 
     handleConfirmBlur = (e) => {
@@ -149,7 +164,7 @@ class RegistrationForm_ extends React.Component {
             <Row>
                 <Col span={12} offset={6} style={{backgroundColor: "white"}}>
                     <Card>
-                        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+                        <Form {...formItemLayout} onSubmit={this.handleSubmit} id="myForm">
                             <Form.Item
                                 label={(
                                     <span>
@@ -162,12 +177,11 @@ class RegistrationForm_ extends React.Component {
                             >
                                 {getFieldDecorator('serial_number', {
                                     rules: [{
-                                        type: 'number', message: 'Please input a number',
-                                    }, {
                                         required: true, message: 'Please input your serial number!',
                                     }],
+                                    initialValue: this.state.sign_up.serial_number,
                                 })(
-                                    <Input id={"serial_number"} onChange={this.onInputChange} value={this.state.sign_up.serial_number || 0}/>
+                                    <Input id={"serial_number"} onChange={getFieldDecorator.setFieldsValue} />
                                 )}
                             </Form.Item>
                             <Form.Item
@@ -175,30 +189,33 @@ class RegistrationForm_ extends React.Component {
                             >
                                 {getFieldDecorator('email', {
                                     rules: [{
-                                        type: 'email', message: 'The input is not valid E-mail!',
+                                        type: 'email', message: 'The input is not a valid E-mail!',
                                     }, {
                                         required: true, message: 'Please input your E-mail!',
                                     }],
+                                    initialValue: this.state.sign_up.email,
                                 })(
-                                    <Input id={"email"} onChange={this.onInputChange} defaultValue={this.state.sign_up.email}/>
+                                    <Input id={"email"} onChange={getFieldDecorator.setFieldsValue} />
                                 )}
                             </Form.Item>
                             <Form.Item
                                 label="First Name"
                             >
-                                {getFieldDecorator('first name', {
+                                {getFieldDecorator('firstname', {
                                     rules: [{ required: true, message: 'Please input your first name!', whitespace: true }],
+                                    initialValue: this.state.sign_up.firstname,
                                 })(
-                                    <Input id={"firstname"} onChange={this.onInputChange} defaultValue={this.state.sign_up.firstname} />
+                                    <Input id={"firstname"} onChange={getFieldDecorator.setFieldsValue} />
                                 )}
                             </Form.Item>
                             <Form.Item
                                 label="Last Name"
                             >
-                                {getFieldDecorator('last name', {
+                                {getFieldDecorator('lastname', {
                                     rules: [{ required: true, message: 'Please input your last name!', whitespace: true }],
+                                    initialValue: this.state.sign_up.lastname,
                                 })(
-                                    <Input id={"lastname"} onChange={this.onInputChange}/>
+                                    <Input id={"lastname"} onChange={getFieldDecorator.setFieldsValue} />
                                 )}
                             </Form.Item>
                             <Form.Item
@@ -210,8 +227,9 @@ class RegistrationForm_ extends React.Component {
                                     }, {
                                         validator: this.validateToNextPassword,
                                     }],
+                                    initialValue: this.state.sign_up.password,
                                 })(
-                                    <Input type="password" id={"password"} onChange={this.onInputChange}/>
+                                    <Input type="password" id={"password"} onChange={getFieldDecorator.setFieldsValue}/>
                                 )}
                             </Form.Item>
                             <Form.Item
@@ -223,65 +241,68 @@ class RegistrationForm_ extends React.Component {
                                     }, {
                                         validator: this.compareToFirstPassword,
                                     }],
+                                    initialValue: this.state.sign_up.password_confirm,
                                 })(
-                                    <Input type="password" onBlur={this.handleConfirmBlur} id={"password_confirm"} onChange={this.onInputChange}/>
+                                    <Input type="password" onBlur={this.handleConfirmBlur} id={"password_confirm"} onChange={getFieldDecorator.setFieldsValue}/>
                                 )}
                             </Form.Item>
                             <div>Address</div>
                             <Form.Item
                                 label="Street"
                             >
-                                {getFieldDecorator('nickname', {
+                                {getFieldDecorator('street', {
                                     rules: [{ required: true, message: 'Please input your last name!', whitespace: true }],
+                                    initialValue: this.state.sign_up.office_address.street,
                                 })(
-                                    <Input id={"street"} onChange={this.onAddressInputChange} />
+                                    <Input id={"street"} onChange={getFieldDecorator.setFieldsValue} />
                                 )}
                             </Form.Item>
                             <Form.Item
                                 label="house_nr"
                             >
-                                {getFieldDecorator('nickname', {
-                                    rules: [{ required: true, message: 'Please input your last name!', whitespace: true }],
+                                {getFieldDecorator('house_nr', {
+                                    rules: [{
+                                        type: 'number', message: 'Please input a number',
+                                    }, {required: true, message: 'Please input your house number'}],
+                                    initialValue: this.state.sign_up.office_address.house_nr,
                                 })(
-                                    <Input id={"house_nr"} onChange={this.onAddressInputChange}/>
+                                    <Input id={"house_nr"} onChange={getFieldDecorator.setFieldsValue}/>
                                 )}
                             </Form.Item>
                             <Form.Item
                                 label="zip"
                             >
-                                {getFieldDecorator('nickname', {
-                                    rules: [{ required: true, message: 'Please input your last name!', whitespace: true }],
+                                {getFieldDecorator('zip', {
+                                    rules: [
+                                        {required: true, type: 'number', min: 5, message: 'Zip has to be 5 caracters long min'}
+                                        ],
+                                    initialValue: this.state.sign_up.office_address.zip,
                                 })(
-                                    <Input id={"zip"} onChange={this.onAddressInputChange}/>
+                                    <Input id={"zip"} onChange={getFieldDecorator.setFieldsValue}/>
                                 )}
                             </Form.Item>
                             <Form.Item
                                 label="city"
                             >
-                                {getFieldDecorator('nickname', {
-                                    rules: [{ required: true, message: 'Please input your last name!', whitespace: true }],
+                                {getFieldDecorator('city', {
+                                    rules: [{ required: true, message: 'Please input the city', whitespace: true }],
+                                    initialValue: this.state.sign_up.office_address.city,
                                 })(
-                                    <Input id={"city"} onChange={this.onAddressInputChange}/>
+                                    <Input id={"city"} onChange={getFieldDecorator.setFieldsValue}/>
                                 )}
                             </Form.Item>
                             <Form.Item
                                 label="country"
                             >
-                                {getFieldDecorator('nickname', {
-                                    rules: [{ required: true, message: 'Please input your last name!', whitespace: true }],
+                                {getFieldDecorator('country', {
+                                    rules: [{ required: true, message: 'Please input the country!', whitespace: true }],
+                                    initialValue: this.state.sign_up.office_address.country,
                                 })(
-                                    <Input id={"country"} onChange={this.onAddressInputChange}/>
+                                    <Input id={"country"} onChange={getFieldDecorator.setFieldsValue}/>
                                 )}
                             </Form.Item>
                             <Form.Item {...tailFormItemLayout}>
-                                {getFieldDecorator('agreement', {
-                                    valuePropName: 'checked',
-                                })(
-                                    <Checkbox>I have read the <a href="">agreement</a></Checkbox>
-                                )}
-                            </Form.Item>
-                            <Form.Item {...tailFormItemLayout}>
-                                <Button type="primary" htmlType="submit">Register</Button>
+                                <Button type="primary"  htmlType="submit">Register</Button>
                             </Form.Item>
 
                         </Form>
